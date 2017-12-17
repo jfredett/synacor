f218b801f58e5c77ad675be7c276cfc2533f9a6d
Joe Fredette <jfredett@gmail.com>
2017-12-17 04:58:56 +0000

    watchman tweaks
    At some point, I will understand how I want to use this.
    
    Also I need more monitors.

--- watchman.sh
+++ watchman.sh
@@ -1,10 +1,18 @@
 clear
 
-watchman-make \
-  -p 'src/bin/assembler.rs' --make "cargo" -t "test --bin syn-asm" -s 2\
-  -p 'src/bin/disassembler.rs' --make "cargo" -t "test --bin syn-dis" -s 2\
-  -p 'src/*.rs' -p 'src/**/*.rs' --make "cargo" -t "test --lib" -s 2 \
-  -p 'src/*.rs' -p 'src/**/*.rs' --make "cargo" -t "build" -s 2 \
-  -p 'Cargo.toml' --make "cargo" -t "update"
+if [ "$1" = builds ] ; then
+
+  watchman-make \
+    -p 'src/*.rs' -p 'src/**/*.rs' --make "cargo" -t "build" -s 2 \
+    -p 'Cargo.toml' --make "cargo" -t "update"
+
+elif [ "$1" = "tests" ] ; then
+
+  watchman-make \
+    -p 'src/bin/assembler.rs' --make "cargo" -t "test --bin syn-asm" -s 2\
+    -p 'src/bin/disassembler.rs' --make "cargo" -t "test --bin syn-dis" -s 2\
+    -p 'src/*.rs' -p 'src/**/*.rs' --make "cargo" -t "test --lib" -s 2
+
+fi
 
 
