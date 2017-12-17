000ae41fdd614a1ccc40b9260dd23c18d4d63d12
Joe Fredette <jfredett@gmail.com>
2017-12-17 06:16:31 +0000

    add vm binary output

--- watchman.sh
+++ watchman.sh
@@ -11,6 +11,7 @@ elif [ "$1" = "tests" ] ; then
   watchman-make \
     -p 'src/bin/assembler.rs' --make "cargo" -t "test --bin syn-asm" -s 2\
     -p 'src/bin/disassembler.rs' --make "cargo" -t "test --bin syn-dis" -s 2\
+    -p 'src/bin/vm.rs' --make "cargo" -t "test --bin syn-vm" -s 2\
     -p 'src/*.rs' -p 'src/**/*.rs' --make "cargo" -t "test --lib" -s 2
 
 fi
