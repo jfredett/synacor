591e6edf25d593d67bdc0ed351badeb937fd6bc7
Joe Fredette <jfredett@gmail.com>
2017-12-17 07:22:07 +0000

    more watchman tweaks

--- watchman.sh
+++ watchman.sh
@@ -4,15 +4,15 @@ if [ "$1" = builds ] ; then
 
   watchman-make \
     -p 'src/*.rs' -p 'src/**/*.rs' --make "clear; cargo" -t "build" -s 5 \
-    -p 'Cargo.toml' --make "cargo" -t "update"
+    -p 'Cargo.toml' --make "clear; cargo" -t "update"
 
 elif [ "$1" = "tests" ] ; then
 
   watchman-make \
-    -p 'src/bin/assembler.rs' --make "cargo" -t "test --bin syn-asm" \
-    -p 'src/bin/disassembler.rs' --make "cargo" -t "test --bin syn-dis" \
+    -p 'src/bin/assembler.rs' --make "clear; cargo" -t "test --bin syn-asm" \
+    -p 'src/bin/disassembler.rs' --make "clear; cargo" -t "test --bin syn-dis" \
     -p 'src/bin/vm.rs' --make "cargo" -t "test --bin syn-vm" \
-    -p 'src/*.rs' -p 'src/**/*.rs' --make "cargo" -t "test --lib"
+    -p 'src/*.rs' -p 'src/**/*.rs' --make "clear; cargo" -t "test --lib"
 
 fi
 
