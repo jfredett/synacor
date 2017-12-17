cef628554de15727dedcd43d97ccfd8ee88cea7b
Joe Fredette <jfredett@gmail.com>
2017-12-17 03:55:38 +0000

    trigger watchman to do builds, updates, etc

--- watchman.sh
+++ watchman.sh
@@ -1,6 +1,8 @@
 watchman-make \
   -p 'src/bin/assembler.rs' --make "clear ; cargo" -t "test --bin syn-asm" -s 2\
   -p 'src/bin/disassembler.rs' --make "clear ; cargo" -t "test --bin syn-dis" -s 2\
-  -p 'src/*.rs' -p 'src/**/*.rs' --make "clear ; cargo" -t "test --lib" -s 2
+  -p 'src/*.rs' -p 'src/**/*.rs' --make "clear ; cargo" -t "test --lib" -s 2 \
+  -p 'src/*.rs' -p 'src/**/*.rs' --make "clear ; cargo" -t "build" -s 2 \
+  -p 'Cargo.toml' --make "clear ; cargo" -t "update"
 
 
